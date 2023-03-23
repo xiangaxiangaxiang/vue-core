b89b6dff7ce978c79c1e6a190675f95b4936b021
cpx <872632899@qq.com>
2023-03-23 10:27:25 +0800

    feat: 解读ref

--- packages/reactivity/src/ref.ts
+++ packages/reactivity/src/ref.ts
@@ -37,6 +37,7 @@ type RefBase<T> = {
   value: T
 }
 
+// 对Ref进行依赖追踪
 export function trackRefValue(ref: RefBase<any>) {
   if (shouldTrack && activeEffect) {
     ref = toRaw(ref)
@@ -52,6 +53,7 @@ export function trackRefValue(ref: RefBase<any>) {
   }
 }
 
+// 触发副作用
 export function triggerRefValue(ref: RefBase<any>, newVal?: any) {
   ref = toRaw(ref)
   const dep = ref.dep
@@ -111,6 +113,7 @@ class RefImpl<T> {
   public readonly __v_isRef = true
 
   constructor(value: T, public readonly __v_isShallow: boolean) {
+    // 判断是否shallow。如果是的话，不需要深层监听，_rowValue和_value直接用value，否则_row使用toRow，_value使用reactive包裹进行深层监听
     this._rawValue = __v_isShallow ? value : toRaw(value)
     this._value = __v_isShallow ? value : toReactive(value)
   }
@@ -123,7 +126,9 @@ class RefImpl<T> {
   set value(newVal) {
     const useDirectValue =
       this.__v_isShallow || isShallow(newVal) || isReadonly(newVal)
+    // 通过以上来判断是否直接用传进来的值，
     newVal = useDirectValue ? newVal : toRaw(newVal)
+    // 判断值是否变更，是的话重新走一遍初始化流程，并触发事件
     if (hasChanged(newVal, this._rawValue)) {
       this._rawValue = newVal
       this._value = useDirectValue ? newVal : toReactive(newVal)
@@ -132,14 +137,17 @@ class RefImpl<T> {
   }
 }
 
+// 强制触发一次副作用
 export function triggerRef(ref: Ref) {
   triggerRefValue(ref, __DEV__ ? ref.value : void 0)
 }
 
+// 获取ref的值，如果不是ref对象则直接返回源对象
 export function unref<T>(ref: T | Ref<T>): T {
   return isRef(ref) ? (ref.value as any) : ref
 }
 
+// 代理一個對象，如果該對象的某個值是ref對象，則自動展開處理，這種代理是淺層的
 const shallowUnwrapHandlers: ProxyHandler<any> = {
   get: (target, key, receiver) => unref(Reflect.get(target, key, receiver)),
   set: (target, key, value, receiver) => {
@@ -156,6 +164,7 @@ const shallowUnwrapHandlers: ProxyHandler<any> = {
 export function proxyRefs<T extends object>(
   objectWithRefs: T
 ): ShallowUnwrapRef<T> {
+  // 如果已经是reactive对象就直接返回
   return isReactive(objectWithRefs)
     ? objectWithRefs
     : new Proxy(objectWithRefs, shallowUnwrapHandlers)
@@ -169,6 +178,7 @@ export type CustomRefFactory<T> = (
   set: (value: T) => void
 }
 
+// 自定义一个ref，手动控制track和trigger，https://cn.vuejs.org/api/reactivity-advanced.html#customref
 class CustomRefImpl<T> {
   public dep?: Dep = undefined
 
@@ -223,11 +233,13 @@ class ObjectRefImpl<T extends object, K extends keyof T> {
   ) {}
 
   get value() {
+    // 维持对源对象的引用
     const val = this._object[this._key]
     return val === undefined ? (this._defaultValue as T[K]) : val
   }
 
   set value(newVal) {
+    // 维持对源对象的引用
     this._object[this._key] = newVal
   }
 
@@ -255,6 +267,7 @@ export function toRef<T extends object, K extends keyof T>(
   defaultValue?: T[K]
 ): ToRef<T[K]> {
   const val = object[key]
+  // 如果已经是ref则直接返回
   return isRef(val)
     ? val
     : (new ObjectRefImpl(object, key, defaultValue) as any)
